6f13541d129fca31af80ed95a9f4b0e9058e95e3
ThrivingDep <119737884+ThrivingDep@users.noreply.github.com>
2023-01-27 19:58:45 +0530

    response/request

--- server.js
+++ server.js
@@ -1,8 +1,95 @@
-const http = require('http');
 
-const server = http.createServer((req,res)=>{
-   if(req==true){
-    console.log('Astha');
-   }
-})
-server.listen(4000);
+// const http = require('http');
+
+// const server = http.createServer((req,res)=>{
+//     console.log(req.url, req.method, req.headers);
+//     // process.exit()
+//     res.setHeader('Content-Type', 'text/html');
+//     res.write('<html>');
+//     res.write('<head><title>My first page</title><head>');
+//     res.write('<body><h1> Hello from my Node.js server!</h1></body>');
+//     res.write('</html>');
+//     res.end();
+// })
+// server.listen(3000);
+
+// for url = home 
+
+
+// const http = require('http');
+// const server = http.createServer((req,res)=>{
+//     console.log(req.url, req.method, req.headers);
+//          res.setHeader('Content-Type', 'text/html');
+//          res.write('<html>');
+//          res.write('<head><title>My first page</title><head>');
+//          res.write('<body><h1> Welcome home</h1></body>');
+//          res.write('</html>');
+//          res.end();
+    
+// })
+
+// server.listen(3000);
+
+
+// for url = /about
+// const http = require('http');
+// const server = http.createServer((req,res)=>{
+//     console.log(req.url, req.method, req.headers);
+//          res.setHeader('Content-Type', 'text/html');
+//          res.write('<html>');
+//          res.write('<head><title>My first page</title><head>');
+//          res.write('<body><h1> Welcome to about us page</h1></body>');
+//          res.write('</html>');
+//          res.end();
+    
+// })
+
+// server.listen(3000);
+
+// for url = /node 
+// const http = require('http');
+// const server = http.createServer((req,res)=>{
+//     console.log(req.url, req.method, req.headers);
+//          res.setHeader('Content-Type', 'text/html');
+//          res.write('<html>');
+//          res.write('<head><title>My first page</title><head>');
+//          res.write('<body><h1> Welcome to my nodejs project</h1></body>');
+//          res.write('</html>');
+//          res.end();
+    
+// })
+
+// server.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
